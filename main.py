2aa090c788f7c5c3e1a33fed81eb478a14261f3e
kalkeesh jami <109733774+kalkeesh@users.noreply.github.com>
2024-06-22 10:17:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     EDA_tasks = ["1.distinguish attributes","2.Data Cleaning", "3.Speed","4.Maps","5.Entry & Exit points"]
     choice = st.sidebar.radio("select tasks:", EDA_tasks)
     #file_format = st.radio('Select file format:', ('csv', 'excel'), key='file_format')
-    # data = st.file_uploader("UPLOAD A DATASET 	:open_file_folder: ")
+    data = st.file_uploader("UPLOAD A DATASET 	:open_file_folder: ")
     data = pd.read_excel('report5.xlsx',skiprows=7)
 
 
